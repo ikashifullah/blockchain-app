16cca552fae63f95b4c19ad52e5afd67d860c243
Kashif Ullah <kashif.ullah@mobilelive.ca>
2023-02-28 19:10:56 -0500

    Add tailwind css functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
-
 import './App.css'
 
 const App = () => {
   return (
     <div className="App">
-      <h1> Welcome to Blockchain, Frontend</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   )
 }
